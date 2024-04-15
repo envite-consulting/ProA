f927c63876a1ed338e0af4693f0277576a3ab611
Philipp Hehnle <philipp.hehnle@envite.de>
2024-04-15 10:55:45 +0200

    Remove jointjs from nav drawer

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -50,10 +50,6 @@ export default defineComponent({
           title: 'Prozesskarte',
           route: '/ProcessMap'
         },
-        {
-          title: 'JointJS',
-          route: '/JointJS'
-        },
       ],
     }),
 
