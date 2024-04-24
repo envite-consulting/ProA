c3f492b96b2bc88f39b3d0a3100ba303d5f689c5
Philipp Hehnle <philipp.hehnle@envite.de>
2024-04-24 15:47:45 +0200

    Fix routing

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -12,7 +12,7 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/ProjectOverviewView.vue'),
+        component: () => import('@/views/Home.vue'),
       },
       {
         path: 'CamundaCloudImport',
