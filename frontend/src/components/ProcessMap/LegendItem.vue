dd89448737e4b3cd723029d50b7a69fbd47f567c
jonaw1 <wagnerxjonathan@gmail.com>
2024-04-26 17:37:54 +0200

    add legend

---
+++ frontend/src/components/ProcessMap/LegendItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list-item>
+    <div class="d-flex flex-row align-center">
+      <svg xmlns="http://www.w3.org/2000/svg" :width="width" :height="height" :viewBox="viewBox">
+        <path :d="path" stroke="#000" :stroke-width="strokeWidth"
+              fill="none"/>
+      </svg>
+      <p class="ms-4">{{ text }}</p>
+    </div>
+  </v-list-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LegendItem',
+  props: {
+    path: String,
+    viewBox: String,
+    text: String,
+    width: String,
+    height: String,
+    strokeWidth: { type: String, default: "5" }
+  }
+});
+</script>
--- frontend/src/components/ProcessMap/ProcessMap.vue
+++ frontend/src/components/ProcessMap/ProcessMap.vue
@@ -2,6 +2,9 @@
   <v-toolbar>
     <v-toolbar-title>{{ selectedProjectName }}</v-toolbar-title>
     <v-spacer></v-spacer>
+    <v-btn icon @click="toggleLegend">
+      <v-icon>mdi-map-legend</v-icon>
+    </v-btn>
     <v-btn icon @click="fetchProcessModels">
       <v-icon>mdi-refresh</v-icon>
     </v-btn>
@@ -9,6 +12,31 @@
   <v-card class="full-screen-below-toolbar" @mouseup="saveGraphState">
     <ProcessDetailDialog ref="processDetailDialog"/>
     <div id="graph-container" class="full-screen"></div>
+    <div style="position: absolute; top: 0; right: 0;">
+      <v-list v-if="showLegend">
+        <v-list-item>
+          <v-list-item-title class="font-weight-bold">Legende</v-list-item-title>
+        </v-list-item>
+        <v-divider></v-divider>
+        <LegendItem text="Prozess" path="M 0 0 h 140 l 10 35 l -10 35 H 0 l 10 -35 l -10 -35 Z" width="30"
+                    height="30" view-box="-2 -2 154 74" stroke-width="8"></LegendItem>
+        <LegendItem text="Datenbank"
+                    path="M 1.5 9 C 15.5 19 75.5 19 90.5 9 l 0 90 c -15 10 -75 10 -89 0 l 0 -90 C 15.5 -1 75.5 -1 90.5 9 v 15 c -15 10 -75 10 -89 0"
+                    width="30"
+                    height="30" view-box="-0.5 -0.5 93 109" stroke-width="5"></LegendItem>
+        <LegendItem text="Startereignis" path="M 10 -20 a 10 10 0 1 0 0.00001 0 Z" width="30"
+                    height="30" view-box="-1.99999 -22 24 24" stroke-width="1.5"></LegendItem>
+        <LegendItem text="Endereignis" path="M 10 -20 a 10 10 0 1 0 0.00001 0 Z" width="30"
+                    height="30" view-box="-1.99999 -22 24 24" stroke-width="3"></LegendItem>
+        <LegendItem text="Zwischenereignis" path="M -25 -10 a 10 10 0 1 0 0.00001 0 Z M -25 -7 a 7 7 0 1 0 0.00001 0 Z"
+                    width="30"
+                    height="30" view-box="-37 -12 24 24" stroke-width="2"></LegendItem>
+        <LegendItem text="Aufrufaktivität"
+                    path="M 35 -10 a3,3 0 0 1 3,3 v15 a3,3 0 0 1 -3,3 h-25 a3,3 0 0 1 -3,-3 v-15 a3,3 0 0 1 3,-3 z"
+                    width="30"
+                    height="30" view-box="5 -12 35 25" stroke-width="2"></LegendItem>
+      </v-list>
+    </div>
     <div class="ma-4" style="position: absolute; bottom: 8px; right: 8px;">
       <v-fab-transition style="margin-right: 5px">
         <v-btn class="mt-auto pointer-events-initial" color="primary" elevation="8" icon="mdi-chevron-left"
@@ -69,6 +97,7 @@ import ProcessDetailDialog from '@/components/ProcessDetailDialog.vue';
 
 import { useAppStore } from "../../store/app";
 import getProject from "../projectService";
+import LegendItem from "@/components/ProcessMap/LegendItem.vue";
 
 const scrollStep = 20;
 
@@ -100,7 +129,6 @@ interface DataStore {
 type DataAccess = "READ" | "WRITE" | "READ_WRITE" | "NONE;";
 
 interface DataStoreConnection {
-
   processid: number
   dataStoreId: number
   access: DataAccess
@@ -109,12 +137,14 @@ interface DataStoreConnection {
 
 export default defineComponent({
   components: {
+    LegendItem,
     ProcessDetailDialog
   },
   data: () => ({
     selectedProjectId: null as number | null,
     selectedProjectName: '' as string,
     store: useAppStore(),
+    showLegend: false
   }),
 
   setup() {
@@ -227,7 +257,6 @@ export default defineComponent({
 
         graph.addCell(abstracProcessShapes);
 
-
         let connectionsShapes = result.data.connections.map((connection: Connection) => {
 
           const link = new shapes.standard.Link();
@@ -249,7 +278,6 @@ export default defineComponent({
         })
 
         graph.addCell(abstractDataStores);
-
 
         let dataStoreConnectionShapes = result.data.dataStoreConnections.map((connection: DataStoreConnection) => {
 
@@ -347,6 +375,9 @@ export default defineComponent({
         default:
           return '';
       }
+    },
+    toggleLegend() {
+      this.showLegend = !this.showLegend;
     }
   }
 })
