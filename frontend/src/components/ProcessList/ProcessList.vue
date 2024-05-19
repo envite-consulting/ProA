cb264a624ee31c09e6c5ee79ed8779f0551514e9
jonaw1 <wagnerxjonathan@gmail.com>
2024-05-19 15:14:37 +0200

    read description from bpmn files

--- frontend/src/components/ProcessList/ProcessList.vue
+++ frontend/src/components/ProcessList/ProcessList.vue
@@ -36,26 +36,33 @@
     </v-fab-transition>
   </div>
 
-  <v-dialog v-model="uploadDialog" persistent width="600">
+  <v-dialog v-model="uploadDialog" persistent width="600" @after-leave="resetUploadDialog">
     <v-card>
       <v-card-title>
-        <span class="text-h5">Prozessmodell hochladen</span>
+        <span class="text-h5">Prozessmodelle hochladen</span>
       </v-card-title>
       <v-card-text>
         <v-container>
-          <v-row>
+          <v-row class="pb-5">
             <v-col cols="12" sm="12" md="12">
-              <v-file-input label="Prozessmodell" v-model="processModel" chips multiple></v-file-input>
+              <v-file-input hide-details label="Prozessmodelle" v-model="processModelFiles" chips multiple
+                            @change="handleFileSelection"></v-file-input>
             </v-col>
-            <v-col cols="12" sm="12" md="12">
-              <v-textarea label="Beschreibung" v-model="description"></v-textarea>
+          </v-row>
+          <v-row v-for="(file, index) in processModelsToUpload" :key="'file-' + index" class="py-5 mt-0">
+            <p class="px-3">{{ file.file.name }}</p>
+            <v-col cols="12" sm="12" md="12" class="py-1">
+              <v-text-field hide-details label="Name" v-model="file.name"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="12" md="12" class="py-1">
+              <v-textarea hide-details rows="3" label="Beschreibung" v-model="file.description"></v-textarea>
             </v-col>
           </v-row>
         </v-container>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue-darken-1" variant="text" @click="uploadDialog = false">
+        <v-btn color="blue-darken-1" variant="text" @click="closeUploadDialog">
           Schließen
         </v-btn>
         <v-btn color="blue-darken-1" variant="text" @click="uploadProcessModel">
@@ -64,28 +71,28 @@
       </v-card-actions>
     </v-card>
   </v-dialog>
+
   <v-dialog v-model="progressDialog" max-width="600">
     <v-card title="Upload">
       <template v-slot:text>
         Lade Prozessmodelle hoch...
         <v-progress-linear color="primary" :model-value="progress" :height="10"></v-progress-linear>
       </template>
-
       <v-card-actions>
         <v-spacer></v-spacer>
-
         <v-btn text="Close" variant="text" @click="progressDialog = false"></v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
-
 </template>
+
 <style></style>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
 import axios from 'axios';
 import ProcessDetailDialog from '@/components/ProcessDetailDialog.vue';
-import { useAppStore } from "../../store/app";
+import { useAppStore } from "@/store/app";
 import getProject from "../projectService";
 
 declare interface ProcessModel {
@@ -96,7 +103,6 @@ declare interface ProcessModel {
 }
 
 export default defineComponent({
-
   components: {
     ProcessDetailDialog
   },
@@ -104,8 +110,8 @@ export default defineComponent({
     uploadDialog: false,
     progressDialog: false,
     progress: 0,
-    description: '',
-    processModel: [] as File[],
+    processModelFiles: [] as File[],
+    processModelsToUpload: [] as { file: File, name: string, description: string }[],
     processModels: [] as ProcessModel[],
     selectedProjectId: null as number | null,
     selectedProjectName: '' as string,
@@ -123,9 +129,7 @@ export default defineComponent({
     })
     this.fetchProcessModels();
   },
-  watch: {},
   methods: {
-
     showProcessInfoDialog(processId: number) {
       (this.$refs.processDetailDialog as InstanceType<typeof ProcessDetailDialog>).showProcessInfoDialog(processId);
     },
@@ -135,39 +139,82 @@ export default defineComponent({
       })
     },
     fetchProcessModels() {
-
       axios.get("/api/project/" + this.selectedProjectId + "/process-model").then(result => {
         this.processModels = result.data;
       })
     },
+    handleFileSelection() {
+      this.processModelsToUpload = [];
 
+      const readFileContent = (file: File) => {
+        return new Promise<string>((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = (event) => {
+            resolve(event.target?.result as string);
+          };
+          reader.onerror = (error) => {
+            reject(error);
+          };
+          reader.readAsText(file);
+        });
+      };
+
+      const parseBPMNContent = (content: string) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(content, "text/xml");
+        const nameElement = xmlDoc.querySelector("bpmn\\:process") || xmlDoc.querySelector("process");
+        const documentationElement = xmlDoc.querySelector("bpmn\\:documentation") || xmlDoc.querySelector("documentation");
+
+        return {
+          name: nameElement ? nameElement.getAttribute("name") || '' : '',
+          description: documentationElement ? documentationElement.textContent || '' : '',
+        };
+      };
+
+      this.processModelFiles.forEach(async (file) => {
+        try {
+          const content = await readFileContent(file);
+          const { name, description } = parseBPMNContent(content);
+          this.processModelsToUpload.push({
+            file,
+            name: name || file.name.replace(/\.[^/.]+$/, ""),
+            description,
+          });
+        } catch (error) {
+          console.error("Error reading file content: ", error);
+        }
+      });
+    },
     async uploadProcessModel() {
-      if (this.processModel !== null && this.processModel.length > 0) {
-
+      if (this.processModelsToUpload.length > 0) {
         this.progressDialog = true;
-        const progressSteps = 100 / (this.processModel.length * 2);
+        const progressSteps = 100 / (this.processModelsToUpload.length * 2);
 
-        for (const processModel of this.processModel) {
+        for (const processModel of this.processModelsToUpload) {
           this.progress += progressSteps;
 
           let formData = new FormData();
-          formData.append("processModel", processModel);
+          formData.append("processModel", processModel.file);
           formData.append("fileName", processModel.name);
-          formData.append("description", this.description);
+          formData.append("description", processModel.description);
 
           await axios.post("/api/project/" + this.selectedProjectId + "/process-model", formData);
 
           this.progress += progressSteps;
         }
         this.fetchProcessModels();
-        this.processModel = [];
-        this.description = '';
-        this.progressDialog = false;
-        this.progress = 0;
-        this.uploadDialog = false;
+        this.closeUploadDialog();
       }
     },
-
+    closeUploadDialog() {
+      this.uploadDialog = false;
+    },
+    resetUploadDialog() {
+      this.processModelFiles = [];
+      this.processModelsToUpload = [];
+      this.progressDialog = false;
+      this.progress = 0;
+    }
   }
 })
 </script>
