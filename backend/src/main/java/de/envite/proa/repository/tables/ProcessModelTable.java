9a8c4ad42cc011aa81056cc16d0f16ab66399796
jonaw1 <wagnerxjonathan@gmail.com>
2024-06-02 11:58:06 +0200

    make description larger in db

--- backend/src/main/java/de/envite/proa/repository/tables/ProcessModelTable.java
+++ backend/src/main/java/de/envite/proa/repository/tables/ProcessModelTable.java
@@ -37,6 +37,7 @@ public class ProcessModelTable {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "processModel")
 	private List<ProcessDataStoreTable> dataStores = new ArrayList<>();
 
+	@Lob
 	@Column
 	private String description;
 	
