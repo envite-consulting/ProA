e17dd1f86d2756a92c198291acf092fb109d1800
jonaw1 <wagnerxjonathan@gmail.com>
2024-10-15 14:14:16 +0200

    add keyboard navigation

--- frontend/src/components/ProcessModel/ProcessModel.vue
+++ frontend/src/components/ProcessModel/ProcessModel.vue
@@ -61,15 +61,18 @@ import { Canvas } from "bpmn-js/lib/features/context-pad/ContextPadProvider";
 import { useAppStore } from "@/store/app";
 import { authHeader } from "@/components/Authentication/authHeader";
 
-const scrollStep = 20;
-
 export default defineComponent({
   data: () => ({
     canvas: null as Canvas | null,
-    store: useAppStore()
+    store: useAppStore(),
+    scrollStep: 20,
+    zoomInMultiplier: 1.1,
+    zoomOutMultiplier: 0.9
   }),
 
   async mounted() {
+    this.addKeydownListener();
+
     const container = document.querySelector('#process-modelling') as HTMLElement;
     const viewer = new NavigatedViewer({
       container
@@ -96,6 +99,10 @@ export default defineComponent({
       }
       this.removeQueryParams();
     }
+  },
+
+  beforeUnmount() {
+    this.removeKeydownListener();
   },
 
   computed: {
@@ -130,30 +137,30 @@ export default defineComponent({
     },
     zoomIn() {
       const currScale = this.canvas.viewbox().scale;
-      this.canvas.zoom(currScale * 1.1, 'auto');
+      this.canvas.zoom(currScale * this.zoomInMultiplier, 'auto');
     },
     zoomOut() {
       const currScale = this.canvas.viewbox().scale;
-      this.canvas.zoom(currScale / 1.1, 'auto');
+      this.canvas.zoom(currScale * this.zoomOutMultiplier, 'auto');
     },
     fitToScreen() {
       this.canvas.zoom('fit-viewport', 'auto');
     },
     goRight() {
       const { x, y, width, height } = this.canvas.viewbox();
-      this.canvas.viewbox({ x: x + scrollStep, y, width, height });
+      this.canvas.viewbox({ x: x - this.scrollStep, y, width, height });
     },
     goLeft() {
       const { x, y, width, height } = this.canvas.viewbox();
-      this.canvas.viewbox({ x: x - scrollStep, y, width, height });
+      this.canvas.viewbox({ x: x + this.scrollStep, y, width, height });
     },
     goUp() {
       const { x, y, width, height } = this.canvas.viewbox();
-      this.canvas.viewbox({ x, y: y - scrollStep, width, height });
+      this.canvas.viewbox({ x, y: y + this.scrollStep, width, height });
     },
     goDown() {
       const { x, y, width, height } = this.canvas.viewbox();
-      this.canvas.viewbox({ x, y: y + scrollStep, width, height });
+      this.canvas.viewbox({ x, y: y - this.scrollStep, width, height });
     },
     removeQueryParams() {
       const url = new URL(window.location.href);
@@ -163,6 +170,37 @@ export default defineComponent({
     goBack() {
       this.removeQueryParams();
       this.$router.go(-1);
+    },
+    addKeydownListener() {
+      window.addEventListener('keydown', this.onKeyDown);
+    },
+    removeKeydownListener() {
+      window.removeEventListener('keydown', this.onKeyDown);
+    },
+    onKeyDown(evt: KeyboardEvent) {
+      switch (evt.key) {
+        case 'ArrowLeft':
+          this.goLeft();
+          break;
+        case 'ArrowRight':
+          this.goRight();
+          break;
+        case 'ArrowUp':
+          this.goUp();
+          break;
+        case 'ArrowDown':
+          this.goDown();
+          break;
+        case '+':
+          this.zoomIn();
+          break;
+        case '-':
+          this.zoomOut();
+          break;
+        case 'f':
+          this.fitToScreen();
+          break;
+      }
     }
   }
 })
