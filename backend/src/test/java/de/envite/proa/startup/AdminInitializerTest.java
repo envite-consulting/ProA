12b659cc89a41d12641d2e4decd99719c31c89b2
Jonathan Wagner <wagnerxjonathan@gmail.com>
2025-01-07 11:39:15 +0100

    Add test for package 'startup'

---
+++ backend/src/test/java/de/envite/proa/startup/AdminInitializerTest.java
@@ -0,0 +1,64 @@
+package de.envite.proa.startup;
+
+import de.envite.proa.entities.User;
+import de.envite.proa.usecases.authentication.AuthenticationUsecase;
+import de.envite.proa.usecases.user.UserUsecase;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+public class AdminInitializerTest {
+
+    private static final String ROLE = "Admin";
+
+    @InjectMocks
+    private AdminInitializer adminInitializer;
+
+    @Mock
+    private UserUsecase userUsecase;
+
+    @Mock
+    private AuthenticationUsecase authenticationUsecase;
+
+    @Inject
+    @ConfigProperty(name = "admin.email")
+    String adminEmail;
+
+    @Inject
+    @ConfigProperty(name = "admin.password")
+    String adminPassword;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testAdminAlreadyExists() {
+        when(userUsecase.findByEmail(adminEmail)).thenReturn(new User());
+
+        adminInitializer.init();
+
+        verify(authenticationUsecase, never()).register(any(User.class));
+    }
+
+    @Test
+    public void testAdminDoesNotExist() {
+        when(userUsecase.findByEmail(adminEmail)).thenReturn(null);
+
+        adminInitializer.init();
+
+        User user = new User();
+        user.setEmail(adminEmail);
+        user.setPassword(adminPassword);
+        user.setRole(ROLE);
+
+        verify(authenticationUsecase, times(1)).register(user);
+    }
+}
