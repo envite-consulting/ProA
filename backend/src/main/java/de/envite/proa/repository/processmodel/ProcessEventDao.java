67e38e3819f014e0da95dad47ede0e95809fdad5
Luca-MarieLimbach <luca-marie.limbach@envite.de>
2025-09-23 11:56:51 +0200

    implement conditional search logic for PostgreSQL and H2

--- backend/src/main/java/de/envite/proa/repository/processmodel/ProcessEventDao.java
+++ backend/src/main/java/de/envite/proa/repository/processmodel/ProcessEventDao.java
@@ -1,19 +1,23 @@
 package de.envite.proa.repository.processmodel;
 
-import java.util.Arrays;
 import java.util.List;
 
 import de.envite.proa.entities.process.EventType;
 import de.envite.proa.repository.tables.ProcessEventTable;
 import de.envite.proa.repository.tables.ProcessModelTable;
 import de.envite.proa.repository.tables.ProjectVersionTable;
+import de.envite.proa.util.SearchLabelBuilder;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @RequestScoped
 public class ProcessEventDao {
+
+    @ConfigProperty(name = "quarkus.datasource.db-kind")
+    String dbKind;
 
 	private EntityManager em;
 
@@ -26,20 +30,33 @@ public class ProcessEventDao {
 	public List<ProcessEventTable> getEventsForLabelAndType(String label, EventType eventType,
 			ProjectVersionTable projectVersionTable) {
 
-        String searchLabel = buildSearchLabel(label);
+		String searchLabel = SearchLabelBuilder.buildSearchLabel(label);
 
-        return em.createQuery(
-                "SELECT e FROM ProcessEventTable e " +
-                    "WHERE e.eventType = :eventType " +
-                    "AND e.project = :project " +
-                    "AND ( e.searchLabel = :searchLabel " +
-                    "OR function('levenshtein', e.searchLabel, :searchLabel) <= 4 )",
-                ProcessEventTable.class)
-                .setParameter("searchLabel", searchLabel)
-                .setParameter("eventType", eventType)
-                .setParameter("project", projectVersionTable)
-                .getResultList();
-    }
+		if ("postgresql".equals(dbKind)) {
+			return em.createQuery(
+							 "SELECT e FROM ProcessEventTable e " +
+								"WHERE e.eventType = :eventType " +
+								"AND e.project = :project " +
+								"AND ( e.searchLabel = :searchLabel " +
+								"OR function('levenshtein', e.searchLabel, :searchLabel) <= 4 )",
+							ProcessEventTable.class)
+					.setParameter("searchLabel", searchLabel)
+					.setParameter("eventType", eventType)
+					.setParameter("project", projectVersionTable)
+					.getResultList();
+		} else {
+			return em.createQuery(
+							 "SELECT e FROM ProcessEventTable e " +
+								"WHERE e.eventType = :eventType " +
+								"AND e.project = :project " +
+								"AND e.searchLabel = :searchLabel ",
+							ProcessEventTable.class)
+					.setParameter("searchLabel", searchLabel)
+					.setParameter("eventType", eventType)
+					.setParameter("project", projectVersionTable)
+					.getResultList();
+		}
+	}
 
 	@Transactional
 	public ProcessEventTable findForProcessModelAndEventType(ProcessModelTable processModel, EventType eventType) {
@@ -53,16 +70,4 @@ public class ProcessEventDao {
 				.findFirst() //
 				.orElse(null);
 	}
-
-    public static String buildSearchLabel(String label) {
-        if (label == null) return null;
-        String cleanedLabel = label
-                .toLowerCase()
-                .replaceAll("[^a-zA-Z0-9\\s]", " ")
-                .trim()
-                .replaceAll("\\s+", " ");
-        String[] words = cleanedLabel.split("\\s+");
-        Arrays.sort(words);
-        return String.join("", words);
-    }
 }
